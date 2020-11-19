6aa1265d663c69dcf942c9bc65a12ccf9a00fa54
Dan <2660574+shirtjs@users.noreply.github.com>
2020-11-19 13:18:42 -0500

    Update index.js

--- goindex-acrou/go2index/index.js
+++ goindex-acrou/go2index/index.js
@@ -67,7 +67,7 @@ var authConfig = {
 var themeOptions = {
   cdn: "https://cdn.jsdelivr.net/gh/shirt-dev/goindex",
   // 主题版本号
-  version: "2.0.8-darkmode-0.2",
+  version: "2.0.8-darkmode-0.1",
   //可选默认系统语言:en/zh-chs/zh-cht
   languages: "en",
   render: {
